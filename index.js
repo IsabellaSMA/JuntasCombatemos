e5cefdf92b9f51969ef118eb15050b4e7b1609db
Isabella Alves <86487337+IsabellaSMA@users.noreply.github.com>
2024-05-27 13:55:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 5173;
+
+app.use(express.static(path.join(__dirname, "src")));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
